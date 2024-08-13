30da78b7f2a0636879b77e0916685b4b37e92881
SaeromByeon <srysmjsr@naver.com>
2024-08-13 16:06:50 +0900

    second commit

---
+++ src/basic/OverflowDemo.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class OverflowDemo {
+  public static void main(String[] args) {
+    int a = 1_000_000;
+    int b = 2_000_000;
+
+    long result = a * (long)b;
+
+    System.out.println(result);
+
+
+  }
+}
